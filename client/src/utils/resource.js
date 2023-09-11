92f0fd63318beab3cfc3ceb69ea2dad985dc725f
tahirshaikh099 <57958180+tahirshaikh099@users.noreply.github.com>
2023-09-11 22:51:48 +0530

    Update resource.js

--- client/src/utils/resource.js
+++ client/src/utils/resource.js
@@ -13,26 +13,41 @@ export const time = [
     { id: "16", t: "04:00pm - 05:00pm" },
     { id: "17", t: "05:00pm - 06:00pm" },
     { id: "18", t: "06:00pm - 07:00pm" },
-];
+]
 
 export async function handleLogin(email, password, navigate) {
-    console.log("email----", email, password);
+    // console.log("email----", email, password);
     let data = { "email": email, "password": password };
-    try {
-        const response = await axios.post('https://appointment-scheduler-bwez.onrender.com/api/signin', data);
-        console.log("response data:- ", response.data.user._id);
-        if (response.error_message) {
-            toast.error(response.error_message);
-        } else {
-            toast.success(response.message);
-            localStorage.setItem("_id", response.data.user._id);
-            localStorage.setItem("_myEmail", response.data.user.email);
-            navigate("/dashboard");
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    if (emailRegex.test(email)) {
+        try {
+            const response = await axios.post('https://appointment-scheduler-bwez.onrender.com/api/signin', data);
+            console.log("response data:- ", response.data.user._id);
+            if (response.error_message) {
+                toast.success("Something went wrong!", {
+                    position: "top-right",
+                    autoClose: 3000, // Close the message after 3 seconds
+                });
+            } else {
+                toast.success("Logged In succesfully", {
+                    position: "top-right",
+                    autoClose: 3000,
+                });
+                localStorage.setItem("_id", response.data.user._id);
+                localStorage.setItem("_myEmail", response.data.user.email);
+                navigate("/dashboard");
+            }
+        } catch (err) {
+            console.error(err);
         }
-    } catch (err) {
-        console.error(err);
+    } else {
+        toast.error("Invalid email address", {
+            position: "top-right",
+            autoClose: 3000,
+        });
     }
 }
+
 export async function handleRegister(email, firstName, lastName, password, navigate) {
     let data = { "firstName": firstName, "lastName": lastName, "email": email, "password": password };
     try {
@@ -40,14 +55,20 @@ export async function handleRegister(email, firstName, lastName, password, navig
         if (response.error_message) {
             toast.error(response.error_message);
         } else {
-            toast.success(response.message);
+            toast.success("User created successfully", {
+                position: "top-right",
+                autoClose: 3000, // Close the message after 3 seconds
+            });
             localStorage.setItem("_id", response.data.user._id);
             localStorage.setItem("_myEmail", response.data.user.email);
             navigate("/dashboard");
         }
     } catch (err) {
         console.error(err);
-        toast.error("Account creation failed");
+        toast.error("Something went wrong", {
+            position: "top-right",
+            autoClose: 3000, // Close the message after 3 seconds
+        });
     }
 }
 
@@ -65,19 +86,32 @@ export async function getAllUser() {
     }
 }
 
-export async function handleAgendaSubmit(title, agenda, email, selectedDate, schedule, withAppointment) {
-    let data = { "title": title, "agenda": agenda, "email": email, "slot_date": selectedDate, "slot_time": schedule, "appointmentWith": withAppointment }
+export async function handleAgendaSubmit(formData) {
+    let data = {
+        "title": formData.title,
+        "agenda": formData.agenda,
+        "slot_date": formData.selectedDate,
+        "email": formData.email,
+        "slot_time": formData.schedule,
+        "name": formData.withAppointment
+    };
+
     console.log("data", data);
     try {
-        const response = await axios.post('https://appointment-scheduler-bwez.onrender.com/api/createAppointments',data);
+        const response = await axios.post('https://appointment-scheduler-bwez.onrender.com/api/createAppointments', data);
         if (response.error_message) {
             toast.error(response.error_message);
+        }else{
+            return toast.success("Agenda submitted successfully",);
         }
         // navigate(`/profile/${localStorage.getItem("_id")}`);
     } catch (err) {
         console.error(err);
     }
 }
-export function getAllAppointment(){
-    
+
+
+
+export function getAllAppointment() {
+
 }
